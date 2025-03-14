7bbfefcdaeb41022cdc75b79a775f49fc726cc6d
noo.noo <ta642379@gmail.com>
2025-03-14 23:15:00 +0700

    feat: project base structure

---
+++ src/main/java/com/startup/skillupbe/SkillUpBeApplication.java
@@ -0,0 +1,13 @@
+package com.startup.skillupbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillUpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillUpBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/startup/skillupbe/SkillUpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.skillupbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillUpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
