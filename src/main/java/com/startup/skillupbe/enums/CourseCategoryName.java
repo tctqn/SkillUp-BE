88ade0268c91a6dec9dbd2bb6ea475d8466e16be
tctqn <trinhtruongcong3@gmail.com>
2025-03-30 17:30:19 +0700

    feat: enhance core models by using enums

---
+++ src/main/java/com/startup/skillupbe/enums/CourseCategoryName.java
@@ -0,0 +1,5 @@
+package com.startup.skillupbe.enums;
+
+public enum CourseCategoryName {
+    PROGRAMMING, DESIGN, MARKETING, BUSINESS, DATA_SCIENCE, AI, FINANCE
+}
--- src/main/java/com/startup/skillupbe/model/CourseRole.java
+++ src/main/java/com/startup/skillupbe/model/CourseRole.java
@@ -1,20 +1,23 @@
 package com.startup.skillupbe.model;
 
+import com.startup.skillupbe.enums.CourseRoleName;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "course_roles")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-@Entity
-@Table(name = "course_roles")
 public class CourseRole {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
-    private String name; // Example: "Instructor", "Student", "Assistant"
+    private CourseRoleName name;
 }
+
